8aa2b48e8719da4153bc5a4f0d77300c685c119a
Rana Emad <ranaemad@Ranas-MacBook-Pro.local>
2020-02-17 17:15:38 +0200

    add routes for contact CRUD operations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
@@ -15,6 +16,8 @@ mongoose.connect("mongodb://localhost:27017/CRMdb",{
 //bodyparser setup
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+routes(app);
 
 app.get('/',(req,res)=>{
     res.send("in home directory");
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,14 @@
+import {addNewContact, getContacts, getContactWithID, updateContact, deleteContact} from '../controllers/crmController';
+
+const routes= (app)=>{
+    app.route("/contact")
+        .get(getContacts)
+        .post(addNewContact);
+    
+    app.route("/contact/:contactID")
+        .get(getContactWithID)
+        .put(updateContact) 
+        .delete(deleteContact);   
+};
+
+export default routes;

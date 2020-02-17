91bb36cb3526e543f9ac94daebdf1f2b547eba0e
Rana Emad <ranaemad@Ranas-MacBook-Pro.local>
2020-02-17 17:00:38 +0200

    create mongoose connection and add body parser

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ import bodyParser from 'body-parser';
 let app= express();
 const PORT=4000;
 
+//mongoose connection
+mongoose.Promise= global.Promise;
+mongoose.connect("mongodb://localhost:27017/CRMdb",{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
+
+//bodyparser setup
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.get('/',(req,res)=>{
     res.send("in home directory");
 });
